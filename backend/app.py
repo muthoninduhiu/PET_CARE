9e8113e355a3159f042aedd15419e6158a3432e1
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-17 13:55:40 +0300

    create a route to get pet_details

--- backend/app.py
+++ backend/app.py
@@ -1,49 +1,20 @@
-from flask import  request, jsonify
-from models import Pet, Appointment,db,app
+from flask import jsonify
+from models import app, Species, PetDetails
 
 
-@app.route('/pets', methods=['POST'])
-def create_pet():
-    data = request.get_json()
-    new_pet = Pet(name=data['name'], breed=data['breed'], age=data['age'], owner=data['owner'])
-    db.session.add(new_pet)
-    db.session.commit()
-    return jsonify({'message': 'New pet added successfully!'})
-
-@app.route('/pets', methods=['GET'])
-def get_all_pets():
-    pets = Pet.query.all()
-    result = []
-    for pet in pets:
-        pet_data = {}
-        pet_data['id'] = pet.id
-        pet_data['name'] = pet.name
-        pet_data['breed'] = pet.breed
-        pet_data['age'] = pet.age
-        pet_data['owner'] = pet.owner
-        result.append(pet_data)
-    return jsonify(result)
-
-@app.route('/appointments', methods=['POST'])
-def create_appointment():
-    data = request.get_json()
-    new_appointment = Appointment(pet_name=data['pet_name'], service=data['service'], date=data['date'])
-    db.session.add(new_appointment)
-    db.session.commit()
-    return jsonify({'message': 'New appointment added successfully!'})
-
-@app.route('/appointments', methods=['GET'])
-def get_all_appointments():
-    appointments = Appointment.query.all()
-    result = []
-    for appointment in appointments:
-        appointment_data = {}
-        appointment_data['id'] = appointment.id
-        appointment_data['pet_name'] = appointment.pet_name
-        appointment_data['service'] = appointment.service
-        appointment_data['date'] = appointment.date
-        result.append(appointment_data)
-    return jsonify(result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/api/pets', methods=['GET'])
+def get_pet_details():
+    pet_details = PetDetails.query.all()
+    results = []
+    for pet_detail in pet_details:
+        species = Species.query.filter_by(id=pet_detail.species_id).first()
+        pet_detail_data = {
+            'pet_id': pet_detail.id,
+            'pet_name': pet_detail.pet_name,
+            'species_name': species.species_name,
+            'life_expectancy': species.life_expectancy,
+            'birth_date': pet_detail.birth_date.strftime('%Y-%m-%d'),
+            'gender': pet_detail.gender
+        }
+        results.append(pet_detail_data)
+    return jsonify(results)
