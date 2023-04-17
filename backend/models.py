b174586417e4a9636a518cd2a15fc4619b666070
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-17 13:55:22 +0300

    add species and pets models

--- backend/models.py
+++ backend/models.py
@@ -1,32 +1,32 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 my_secret_key = os.getenv("USER_PASSWORD")
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:{}@localhost/pet_care_db'.format(my_secret_key)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:{}@localhost:5432/pet_care_db'.format(my_secret_key)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-class Pet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    breed = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    owner = db.Column(db.String(100), nullable=False)
+with app.app_context():
+    class Species(db.Model):
+        __tablename__ = 'species'
+        id = db.Column(db.Integer, primary_key=True)
+        species_name = db.Column(db.String(120))
+        life_expectancy = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f'<Pet {self.id}>'
 
-class Appointment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pet_name = db.Column(db.String(100), nullable=False)
-    service = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    class PetDetails(db.Model):
+        __tablename__ = 'pet_details'
+        id = db.Column(db.Integer, primary_key=True)
+        pet_name = db.Column(db.String(120))
+        birth_date = db.Column(db.Date)
+        gender = db.Column(db.String(120))
+        species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
+        species = db.relationship('Species', backref='pet_details')
 
-    def __repr__(self):
-        return f'<Appointment {self.id}>'
