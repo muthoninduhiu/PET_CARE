96016834fbe661d012cf5eb830a118ebcbab56b6
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-05-25 12:39:10 +0300

    improve the dashboard

--- pet_care_frontend/src/Components/Home.js
+++ pet_care_frontend/src/Components/Home.js
@@ -5,25 +5,28 @@ import "../CSS/Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-heading">Welcome to the Pet Haven System Dashboard</h1>
+      <h1 className="home-heading display-4 text-center">
+        Welcome to Pet Haven's Dashboard
+      </h1>
       <section className="home-section">
-        <h5>Start by exploring the following sections:</h5>
-        <div className="dashboard-buttons">
-          <Link to="/pets" className="dashboard-button">
+        <h5 className="text-center">Start by exploring the following sections:</h5>
+        <div className="dashboard-buttons d-flex justify-content-center">
+          <Link to="/pets" className="dashboard-button btn btn-primary">
             View Pets
           </Link>
-          <Link to="/appointments" className="dashboard-button">
+          <Link to="/appointments" className="dashboard-button btn btn-primary">
             View Appointments
           </Link>
-          <Link to="/services" className="dashboard-button">
+          <Link to="/services" className="dashboard-button btn btn-primary">
             View Services
           </Link>
-          <Link to="/reports" className="dashboard-button">
+          <Link to="/reports" className="dashboard-button btn btn-primary">
             View Reports
           </Link>
         </div>
       </section>
     </div>
+
   );
 }
 
