106b6a79d5a12e1db74184f808bb8828b08aedf0
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-05-25 12:37:29 +0300

    add team members

--- pet_care_frontend/src/Components/About.js
+++ pet_care_frontend/src/Components/About.js
@@ -1,31 +1,52 @@
-import React from "react";
-// import "../CSS/Home.css";
-import Images from "../images/3.jpg";
+import React from 'react';
+import '../CSS/About.css'; // Import the CSS file for styling
+import image1 from '../images/2.jpg'
+import image2 from '../images/3.jpg'
+import image3 from '../images/thumbs.jpg'
+const About = () => {
+  const staffMembers = [
+    {
+      name: 'John Doe',
+      role: 'CEO',
+      image: image1,
+    },
+    {
+      name: 'Jane Smith',
+      role: 'Head of Product',
+      image: image2,
+    },
+    {
+      name: 'Peter Parker',
+      role: 'Web Developer',
+      image: image3,
+    },
+    // Add more staff members as needed
+  ];
 
-function Home() {
   return (
-    <div className="home-container">
-     <h1 style={{marginTop: 20 + 'px'}}>About Us</h1>
-      <section>
-        <div class="container">
-          <div class="row mt-5 jalign-self-center">
-            <div class="col-8 ">
-              <p className="m-4 py-5">
-              I am passionate about technology and innovation.
-              I believe that everyone should have the opportunity to learn and succeed in the tech industry,
-              regardless of their gender, race, or background.
-              I believe that a more diverse and inclusive tech industry will benefit everyone,
-              leading to more innovative solutions, better products, and stronger communities.
-              </p>
-            </div>
-            <div class="col-4">
-              <img src={Images} class="card-img-top" alt="..."></img>
-            </div>
+    <section className="about-us">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our personal care system! We are dedicated to providing you with the best products and services to help you take care of yourself and enhance your well-being.
+      </p>
+      <p>
+        Our team of experts is passionate about personal care and is committed to delivering high-quality solutions tailored to your needs. Whether you're looking for skincare, haircare, or general wellness products, we've got you covered.
+      </p>
+      <p>
+        We believe that self-care is essential for a balanced and healthy lifestyle. That's why we strive to offer a wide range of products that are both effective and sustainable. We carefully select our suppliers to ensure that our offerings are ethically sourced and environmentally friendly.
+      </p>
+      <h3>Our Team</h3>
+      <div className="staff-members">
+        {staffMembers.map((member, index) => (
+          <div key={index} className="staff-member">
+            <img src={member.image} alt={member.name} />
+            <h4>{member.name}</h4>
+            <p>{member.role}</p>
           </div>
-        </div>
-      </section>
-    </div>
+        ))}
+      </div>
+    </section>
   );
-}
+};
 
-export default Home;
+export default About;
